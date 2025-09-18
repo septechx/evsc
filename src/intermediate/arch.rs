2530abd5ee2e7d37ca07ca6aa267bd1aeae3d2b6
Sep <septechx@proton.me>
2025-09-18 17:54:26 +0200

    refactor: Move runtime to separate file

--- src/intermediate/arch.rs
+++ src/intermediate/arch.rs
@@ -1,6 +1,3 @@
-use anyhow::Result;
-use inkwell::{builder::Builder, context::Context, values::BasicValueEnum, InlineAsmDialect};
-
 use crate::{
     errors::{CompilationError, ErrorLevel},
     ERRORS,
@@ -20,39 +17,4 @@ pub fn is_64() -> bool {
             unreachable!()
         }
     }
-}
-
-pub fn create_exit_syscall<'ctx>(
-    context: &'ctx Context,
-    builder: &Builder<'ctx>,
-    exit_code: BasicValueEnum<'ctx>,
-) -> Result<()> {
-    let (asm, exit_ty) = if is_64() {
-        let exit_ty = context
-            .void_type()
-            .fn_type(&[context.i64_type().into()], false);
-        let asm = "mov rdi, $0\nmov rax, 60\nsyscall".to_string();
-        (asm, exit_ty)
-    } else {
-        let exit_ty = context
-            .void_type()
-            .fn_type(&[context.i32_type().into()], false);
-        let asm = "mov ebx, $0\nmov eax, 60\nint 0x80".to_string();
-        (asm, exit_ty)
-    };
-
-    let inline = context.create_inline_asm(
-        exit_ty,
-        asm,
-        "r,~{rax},~{rdi},~{rcx},~{r11},~{memory}".to_string(),
-        true,
-        false,
-        Some(InlineAsmDialect::Intel),
-        false,
-    );
-    builder.build_indirect_call(exit_ty, inline, &[exit_code.into()], "exit_call")?;
-
-    builder.build_unreachable()?;
-
-    Ok(())
 }

0d58beff3e02aa22e609f54f11073aef50d7e7bf
Sep <septechx@proton.me>
2026-01-02 03:15:48 +0100

    feat: Implement Visitor/Vistable API for AST validation

--- src/ast/visit.rs
+++ src/ast/visit.rs
@@ -1,4 +1,8 @@
-use crate::ast::{Ast, Expr, Stmt, Type, statements::BlockStmt};
+use std::{collections::HashMap, hash::Hash};
+
+use crate::ast::{
+    Ast, Expr, ExprKind, Stmt, StmtKind, Type, TypeKind, expressions::*, statements::*, types::*,
+};
 
 pub trait Visitor {
     fn visit_stmt(&mut self, stmt: &mut Stmt);
@@ -10,20 +14,307 @@ pub trait Visitable {
     fn visit(&mut self, visitor: &mut impl Visitor);
 }
 
-impl Visitable for BlockStmt {
+impl<T: Visitable> Visitable for Box<T> {
     fn visit(&mut self, visitor: &mut impl Visitor) {
-        visit_stmts(&mut self.body, visitor);
+        self.as_mut().visit(visitor);
+    }
+}
+
+impl<T: Visitable> Visitable for Option<T> {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        if let Some(inner) = self {
+            inner.visit(visitor);
+        }
+    }
+}
+
+impl<T: Visitable> Visitable for Vec<T> {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        for inner in self {
+            inner.visit(visitor);
+        }
+    }
+}
+
+impl<K: Eq + Hash, V: Visitable> Visitable for HashMap<K, V> {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        for (_, value) in self.iter_mut() {
+            value.visit(visitor);
+        }
     }
 }
 
 impl Visitable for Ast {
     fn visit(&mut self, visitor: &mut impl Visitor) {
-        visit_stmts(&mut self.0, visitor);
+        for stmt in &mut self.0 {
+            stmt.visit(visitor);
+        }
     }
 }
 
-fn visit_stmts(stmts: &mut [Stmt], _visitor: &mut impl Visitor) {
-    for _stmt in stmts.iter_mut() {
-        todo!("Call .visit + implement Visitable")
+impl Visitable for Stmt {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        visitor.visit_stmt(self);
+
+        match &mut self.kind {
+            StmtKind::Block(block) => block.visit(visitor),
+            StmtKind::Expression(expr_stmt) => expr_stmt.visit(visitor),
+            StmtKind::VarDecl(var) => var.visit(visitor),
+            StmtKind::StructDecl(s) => s.visit(visitor),
+            StmtKind::InterfaceDecl(i) => i.visit(visitor),
+            StmtKind::FnDecl(f) => f.visit(visitor),
+            StmtKind::Return(r) => r.visit(visitor),
+            StmtKind::Import(_) => {}
+        }
+    }
+}
+
+impl Visitable for BlockStmt {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        self.body.visit(visitor);
+    }
+}
+
+impl Visitable for ExpressionStmt {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        self.expression.visit(visitor);
+    }
+}
+
+impl Visitable for VarDeclStmt {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        if let Some(val) = &mut self.assigned_value {
+            val.visit(visitor);
+        }
+        self.type_.visit(visitor);
+    }
+}
+
+impl Visitable for StructProperty {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        self.type_.visit(visitor);
+    }
+}
+
+impl Visitable for StructMethod {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        self.fn_decl.visit(visitor);
+    }
+}
+
+impl Visitable for StructDeclStmt {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        for p in &mut self.properties {
+            p.visit(visitor);
+        }
+        for m in &mut self.methods {
+            m.visit(visitor);
+        }
+    }
+}
+
+impl Visitable for InterfaceMethod {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        self.fn_decl.visit(visitor);
+    }
+}
+
+impl Visitable for InterfaceDeclStmt {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        for m in &mut self.methods {
+            m.visit(visitor);
+        }
+    }
+}
+
+impl Visitable for FnArgument {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        self.type_.visit(visitor);
+    }
+}
+
+impl Visitable for FnDeclStmt {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        for arg in &mut self.arguments {
+            arg.visit(visitor);
+        }
+        for stmt in &mut self.body {
+            stmt.visit(visitor);
+        }
+        self.return_type.visit(visitor);
+    }
+}
+
+impl Visitable for ReturnStmt {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        if let Some(v) = &mut self.value {
+            v.visit(visitor);
+        }
+    }
+}
+
+impl Visitable for Expr {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        visitor.visit_expr(self);
+
+        match &mut self.kind {
+            ExprKind::Number(_) => {}
+            ExprKind::String(_) => {}
+            ExprKind::Symbol(_) => {}
+            ExprKind::Binary(bin) => {
+                bin.left.visit(visitor);
+                bin.right.visit(visitor);
+            }
+            ExprKind::Postfix(p) => {
+                p.left.visit(visitor);
+            }
+            ExprKind::Prefix(p) => {
+                p.right.visit(visitor);
+            }
+            ExprKind::Assignment(a) => {
+                a.assigne.visit(visitor);
+                a.value.visit(visitor);
+            }
+            ExprKind::StructInstantiation(si) => {
+                si.properties.visit(visitor);
+            }
+            ExprKind::ArrayLiteral(arr) => {
+                arr.underlying.visit(visitor);
+                arr.contents.visit(visitor);
+            }
+            ExprKind::FunctionCall(fc) => {
+                fc.callee.visit(visitor);
+                fc.arguments.visit(visitor);
+            }
+            ExprKind::MemberAccess(ma) => {
+                ma.base.visit(visitor);
+            }
+            ExprKind::Type(t) => {
+                t.underlying.visit(visitor);
+            }
+            ExprKind::As(asexpr) => {
+                asexpr.expr.visit(visitor);
+                asexpr.ty.visit(visitor);
+            }
+        }
+    }
+}
+
+impl Visitable for NumberExpr {
+    fn visit(&mut self, _visitor: &mut impl Visitor) {}
+}
+impl Visitable for StringExpr {
+    fn visit(&mut self, _visitor: &mut impl Visitor) {}
+}
+impl Visitable for SymbolExpr {
+    fn visit(&mut self, _visitor: &mut impl Visitor) {}
+}
+
+impl Visitable for BinaryExpr {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        self.left.visit(visitor);
+        self.right.visit(visitor);
+    }
+}
+impl Visitable for PostfixExpr {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        self.left.visit(visitor);
+    }
+}
+impl Visitable for PrefixExpr {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        self.right.visit(visitor);
+    }
+}
+impl Visitable for AssignmentExpr {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        self.assigne.visit(visitor);
+        self.value.visit(visitor);
+    }
+}
+impl Visitable for StructInstantiationExpr {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        self.properties.visit(visitor);
+    }
+}
+impl Visitable for ArrayLiteralExpr {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        self.underlying.visit(visitor);
+        self.contents.visit(visitor);
+    }
+}
+impl Visitable for FunctionCallExpr {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        self.callee.visit(visitor);
+        self.arguments.visit(visitor);
+    }
+}
+impl Visitable for MemberAccessExpr {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        self.base.visit(visitor);
+    }
+}
+impl Visitable for TypeExpr {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        self.underlying.visit(visitor);
+    }
+}
+impl Visitable for AsExpr {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        self.expr.visit(visitor);
+        self.ty.visit(visitor);
+    }
+}
+
+impl Visitable for Type {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        visitor.visit_type(self);
+        match &mut self.kind {
+            TypeKind::Symbol(_) => {}
+            TypeKind::Pointer(p) => p.underlying.visit(visitor),
+            TypeKind::Slice(s) => s.underlying.visit(visitor),
+            TypeKind::FixedArray(f) => {
+                f.underlying.visit(visitor);
+            }
+            TypeKind::Mut(m) => m.underlying.visit(visitor),
+            TypeKind::Function(ft) => {
+                ft.parameters.visit(visitor);
+                ft.return_type.visit(visitor);
+            }
+            TypeKind::Infer => {}
+            TypeKind::Never => {}
+        }
+    }
+}
+
+impl Visitable for SymbolType {
+    fn visit(&mut self, _visitor: &mut impl Visitor) {}
+}
+impl Visitable for PointerType {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        self.underlying.visit(visitor);
+    }
+}
+impl Visitable for SliceType {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        self.underlying.visit(visitor);
+    }
+}
+impl Visitable for FixedArrayType {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        self.underlying.visit(visitor);
+    }
+}
+impl Visitable for MutType {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        self.underlying.visit(visitor);
+    }
+}
+impl Visitable for FunctionType {
+    fn visit(&mut self, visitor: &mut impl Visitor) {
+        for p in &mut self.parameters {
+            p.visit(visitor);
+        }
+        self.return_type.visit(visitor);
     }
 }

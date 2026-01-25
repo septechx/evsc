b90a590310a48d18917d94e705d6a59e79057fa8
Sep <septechx@proton.me>
2026-01-25 18:35:11 +0100

    chore: Add interner tests

--- src/hir/interner.rs
+++ src/hir/interner.rs
@@ -27,3 +27,51 @@ impl Interner {
         &self.vec[sym as usize]
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_intern_identical_strings() {
+        let mut interner = Interner::new();
+        let sym1 = interner.intern("hello");
+        let sym2 = interner.intern("hello");
+        assert_eq!(sym1, sym2);
+    }
+
+    #[test]
+    fn test_intern_different_strings() {
+        let mut interner = Interner::new();
+        let sym1 = interner.intern("hello");
+        let sym2 = interner.intern("world");
+        assert_ne!(sym1, sym2);
+    }
+
+    #[test]
+    fn test_lookup() {
+        let mut interner = Interner::new();
+        let sym = interner.intern("hello");
+        assert_eq!(interner.lookup(sym), "hello");
+    }
+
+    #[test]
+    fn test_intern_multiple_lookups() {
+        let mut interner = Interner::new();
+        let s1 = "apple";
+        let s2 = "banana";
+        let s3 = "cherry";
+
+        let sym1 = interner.intern(s1);
+        let sym2 = interner.intern(s2);
+        let sym3 = interner.intern(s3);
+
+        assert_eq!(interner.lookup(sym1), s1);
+        assert_eq!(interner.lookup(sym2), s2);
+        assert_eq!(interner.lookup(sym3), s3);
+
+        assert_eq!(interner.intern(s1), sym1);
+        assert_eq!(interner.intern(s2), sym2);
+        assert_eq!(interner.intern(s3), sym3);
+    }
+}
